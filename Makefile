fc18070799a0dd0ba5f992c7ce405ae03684071d
Brent Westbrook <brentrwestbrook@gmail.com>
2024-04-23 12:04:06 -0400

    add makefile for profiling and recap benchmark

---
+++ Makefile
@@ -0,0 +1,7 @@
+profile = RUSTFLAGS='-g' cargo build --release --bin $(1); \
+	valgrind --tool=callgrind --callgrind-out-file=callgrind.out	\
+		--collect-jumps=yes --simulate-cache=yes		\
+		target/release/$(1)
+
+prof:
+	$(call profile,recap)
---
+++ src/bin/recap.rs
@@ -0,0 +1,13 @@
+use rdkit_rs::{fragment::recap_decompose, ROMol};
+
+// the smiles in main right now took ~100 ms in my chembl code. this one
+// took 50.4 seconds
+
+// [H]OC([H])([H])[C@]1([H])O[C@]([H])(O[C@@]2([H])[C@]([H])(C([H])([H])[H])O[C@@]([H])(C([H])([H])OC([H])([H])[C@@]3([H])[C@@]([H])(OC([H])([H])[H])C([H])([H])[C@]([H])(C([H])([H])OC([H])([H])[C@]4([H])[C@]([H])(C([H])([H])[H])O[C@@]([H])(C([H])([H])OC([H])([H])[C@@]5([H])[C@@]([H])(OC([H])([H])[H])C([H])([H])[C@]([H])(O[C@]6([H])C([H])([H])C7=C([H])C([H])([H])[C@]8(O[H])[C@]([H])(C([H])([H])[C@@]([H])(OC(=O)/C([H])=C(\[H])c9c([H])c([H])c([H])c([H])c9[H])[C@@]9(C([H])([H])[H])[C@]8(O[H])C([H])([H])C([H])([H])[C@@]9(O[H])C(=O)C([H])([H])[H])[C@@]7(C([H])([H])[H])C([H])([H])C6([H])[H])O[C@]5([H])C([H])([H])[H])C([H])([H])[C@]4([H])OC([H])([H])[H])O[C@]3([H])C([H])([H])[H])C([H])([H])[C@@]2([H])OC([H])([H])[H])[C@@]([H])(O[H])[C@]([H])(O[H])[C@@]1([H])O[H]
+
+fn main() {
+    let smiles = "CCO";
+    let mol = ROMol::from_smiles(smiles);
+    let leaves = recap_decompose(&mol, None, Some(4), None).get_leaves();
+    println!("found {} leaves", leaves.len());
+}
