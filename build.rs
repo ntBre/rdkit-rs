713c887f1df721577fa2ba316a790c1af4caed7b
Brent Westbrook <brentrwestbrook@gmail.com>
2024-04-23 11:57:52 -0400

    add to rpath for building test binaries

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
     let include = std::env::var("DEP_SHIM_INCLUDE").unwrap();
-    println!("cargo:include={include}")
+    println!("cargo:include={include}");
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{include}");
 }
